3c30b7a2822a360a56ae8b106e7960b10137d0d2
Rajat Sharan <errajatsharan@gmail.com>
2025-06-28 18:08:31 +0530

    Added practice task under task folder for Promise and async/await flow

--- 01-javascript-essentials/Practice/03-async-js/async-practice.js
+++ 01-javascript-essentials/Practice/03-async-js/async-practice.js
@@ -1,41 +1,35 @@
-// function fetchName(){
-//     return new Promise((resolve)=>{
+function fetchName(){
+    return new Promise((resolve)=>{
 
-//          console.log("Waiting for response")
+         console.log("Waiting for response")
 
 
-//         setTimeout(()=>{
+        setTimeout(()=>{
 
-//             resolve("Rajat")
+            resolve("Rajat")
            
-//         },2000)
+        },2000)
 
-//     })
-// }
-
-// // async function showName(){
-
-// //     const name = await fetchName()
-// //     console.log(" Name " + name)
-
-// // }
-
-
-// function showName(){
-//     fetchName().then((resolve)=>{
-
-//         console.log(" Name " + resolve)
-//     })
-// }
-
-// showName()
-
-
-
-function loginUser() {
-   return new Promise((resolve) => {
-       setTimeout(() => resolve("Logged in as Sidharth"), 6000);
-   });
+    })
 }
+
+// async function showName(){
+
+//     const name = await fetchName()
+//     console.log(" Name " + name)
+
+// }
+
+
+function showName(){
+    fetchName().then((resolve)=>{
+
+        console.log(" Name " + resolve)
+    })
+}
+
+showName()
+
+
 
 
